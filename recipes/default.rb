4cacc80f68184eeef257d2b4a4ffecdeeb1c6cf3
Melnikov Sergey <me@rnr.name>
2014-06-10 11:00:55 +0700

    Added cli configure.

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,9 +22,17 @@ template "#{node['deploy-project']['path']}/app/webroot/.htaccess" do
   group node['apache']['group']
 end
 
-
 file "/etc/php5/apache2/conf.d/20-timezone.ini" do
   content "date.timezone = #{node['cake']['timezone']}"
   notifies :restart, 'service[apache2]', :delayed
   not_if { node['cake']['timezone'].nil? }
+end
+
+['/etc/php5/apache2/conf.d/20-timezone.ini',
+ '/etc/php5/cli/conf.d/20-timezone.ini'].each do |fn|
+  file fn do
+    content "date.timezone = #{node['cake']['timezone']}"
+    notifies :restart, 'service[apache2]', :delayed
+    not_if { node['cake']['timezone'].nil? }
+  end
 end
