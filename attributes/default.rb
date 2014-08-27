993f1fc4f6197c30098b42fef3906b02ac6e726b
Melnikov Sergey <me@rnr.name>
2014-08-27 11:44:31 +0700

    Added Session configure.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,7 @@ default['cake']['debug'] = false
 default['cake']['cache.disable'] = false
 # Move to deploy project
 default['deploy-project']['base'] = '/'
+
+default['cake']['Session']['defaults'] = 'php'
+default['cake']['Session']['timeout'] = 60
+default['cake']['Session']['autoRegenerate'] = true
