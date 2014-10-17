5ec13affc5568ccf0d540bc1316592855352a595
Melnikov Sergey <me@rnr.name>
2014-10-17 12:36:10 +0700

    Added import database from file

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,22 @@
+unless node['deploy-project']['db']['install'].nil?
+  db_name = node['deploy-project']['db']['database'] || node['deploy-project']['project']
+  command =
+      if node['deploy-project']['db']['password'].nil? || (node['deploy-project']['db']['password'] == '')
+        "mysql --host='#{node['deploy-project']['db']['host']}' -u#{node['deploy-project']['db']['user']} #{db_name} < '#{node['deploy-project']['db']['install']}'"
+      else
+        "mysql --host='#{node['deploy-project']['db']['host']}' -u#{node['deploy-project']['db']['user']} -p#{node['deploy-project']['db']['password']} #{db_name} < '#{node['deploy-project']['db']['install']}'"
+      end
+
+  execute command do
+    cwd node['deploy-project']['path']
+    not_if { (::File.exists?("#{node['deploy-project']['path']}/app/Config/database.php") &&
+        (node['deploy-project']['db']['install_type'] != 'force')) ||
+        !::File.exists?(node['deploy-project']['db']['install']) ||
+            (node['deploy-project']['db']['install_type'] == 'none')
+    }
+  end
+end
+
 template "#{node['deploy-project']['path']}/app/Config/core.php" do
   source 'php-cake-core.php.erb'
   owner node['apache']['user']
@@ -48,7 +67,6 @@ end
     group node['apache']['group']
   end
 end
-
 
 execute 'echo "n\ny\n" | app/Console/cake schema create' do
   cwd node['deploy-project']['path']
